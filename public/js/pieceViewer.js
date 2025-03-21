247a01fdca60baf7a17dd29f5f4c904412fa0f80
llesueur2024 <leo.lesueur2024@campus-eni.fr>
2025-03-21 10:10:36 +0100

    working animation selector

--- public/js/pieceViewer.js
+++ public/js/pieceViewer.js
@@ -1,5 +1,33 @@
+const animations = [
+    "noAnimation",
+    "breathing"
+]
+
 const pieceBodies = [...document.getElementsByClassName('pieceBody')]
-.map(elem => [...elem.childNodes]);
+.map(elem => [...elem.childNodes]
+    .filter(obj => obj.innerText)
+);
+
+const changeAnimation = (pieceBody, animation) => {
+    for (const row of pieceBody) {
+        row.classList.replace([...row.classList][1], animation);
+    }
+}
+
+//load animations in the animation selectors
+const animationsSelectors = [...document.getElementsByClassName('animationSelector')];
+animationsSelectors.forEach((selector, index) => {
+    for (const animation of animations) {
+        const opt = document.createElement('option');
+        opt.value = animation;
+        opt.innerHTML = animation;
+        selector.appendChild(opt);
+    }
+    selector.addEventListener('change', function() {
+        changeAnimation(pieceBodies[index], this.value)
+    });
+})
 
 
-console.log(pieceBodies);
+
+
